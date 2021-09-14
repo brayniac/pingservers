761dba43f15c344976d1daad0c82b27391c3d188
Brian Martin <bmartin@twitter.com>
2021-09-14 15:29:05 -0700

    first commit

---
+++ src/mio.rs
@@ -0,0 +1,132 @@
+use mio::event::Event;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Registry, Token};
+use std::collections::HashMap;
+use std::io::{self, Read, Write};
+
+const SERVER: Token = Token(0);
+
+const ADDR: &str = "127.0.0.1:12321";
+
+struct Session {
+    connection: TcpStream,
+    buffer: Vec<u8>,
+}
+
+fn main() -> io::Result<()> {
+    println!("starting pingserver_mio");
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(1024);
+
+
+    let addr = ADDR.parse().unwrap();
+    let mut server = TcpListener::bind(addr)?;
+
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE)?;
+
+    let mut connections = HashMap::new();
+    let mut unique_token = Token(SERVER.0 + 1);
+
+    println!("listening on: {}", ADDR);
+
+    loop {
+        poll.poll(&mut events, None)?;
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => loop {
+                    let (mut connection, _address) = match server.accept() {
+                        Ok((connection, address)) => (connection, address),
+                        Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            break;
+                        }
+                        Err(e) => {
+                            return Err(e);
+                        }
+                    };
+
+                    let token = next(&mut unique_token);
+                    poll.registry().register(
+                        &mut connection,
+                        token,
+                        Interest::READABLE,
+                    )?;
+
+                    let session = Session {
+                        connection,
+                        buffer: vec![0; 1024],
+                    };
+
+                    connections.insert(token, session);
+                },
+                token => {
+                    let done = if let Some(connection) = connections.get_mut(&token) {
+                        handle_connection_event(poll.registry(), connection, event)?
+                    } else {
+                        false
+                    };
+                    if done {
+                        connections.remove(&token);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn next(current: &mut Token) -> Token {
+    let next = current.0;
+    current.0 += 1;
+    Token(next)
+}
+
+/// Returns `true` if the connection is done.
+fn handle_connection_event(
+    registry: &Registry,
+    session: &mut Session,
+    event: &Event,
+) -> io::Result<bool> {
+    if event.is_readable() {
+        let mut connection_closed = false;
+        let mut bytes_read = 0;
+
+        loop {
+            match session.connection.read(&mut session.buffer) {
+                Ok(0) => {
+                    connection_closed = true;
+                    break;
+                }
+                Ok(n) => {
+                    bytes_read += n;
+                    if bytes_read == session.buffer.len() {
+                        session.buffer.resize(session.buffer.len() + 1024, 0);
+                    }
+                }
+                Err(ref err) if would_block(err) => break,
+                Err(ref err) if interrupted(err) => continue,
+                Err(err) => return Err(err),
+            }
+        }
+
+        if &session.buffer[0..6] == b"PING\r\n" || &session.buffer[0..6] == b"ping\r\n" {
+            session.connection.write_all(b"PONG\r\n")?;
+            session.buffer.write_all(&[0,0,0,0,0,0])?;
+        }
+
+        if connection_closed {
+            println!("Connection closed");
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
+}
+
+fn would_block(err: &io::Error) -> bool {
+    err.kind() == io::ErrorKind::WouldBlock
+}
+
+fn interrupted(err: &io::Error) -> bool {
+    err.kind() == io::ErrorKind::Interrupted
+}
---
+++ src/smol.rs
@@ -0,0 +1,46 @@
+use smol::io::AsyncWriteExt;
+use smol::io::AsyncReadExt;
+use std::net::TcpListener;
+
+use smol::{io, Async};
+
+fn main() -> io::Result<()> {
+    println!("starting pingserver_smol");
+    smol::block_on(async {
+        // Create a listener.
+        let listener = Async::<TcpListener>::bind(([127, 0, 0, 1], 12321))?;
+        println!("Listening on {}", listener.get_ref().local_addr()?);
+
+        // Accept clients in a loop.
+        loop {
+            let (mut socket, peer_addr) = listener.accept().await?;
+            println!("Accepted client: {}", peer_addr);
+
+            // Spawn a task that echoes messages from the client back to it.
+            smol::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                if n == 0 {
+                    return;
+                }
+
+                if buf.len() < 6 {
+                    continue;
+                }
+
+                if &buf[0..6] == b"PING\r\n" || &buf[0..6] == b"ping\r\n" {
+                    socket.write_all(b"PONG\r\n").await.expect("failed to write response to socket");
+                    buf.write_all(&[0,0,0,0,0,0]).await.expect("failed to clear buffer");
+                }
+            }
+        }).detach();
+        }
+    })
+}
